d52e24ab587e411edb3b3da1b07431ac65ef83c6
Joseph Boateng <josephboatengk7@gmail.com>
2026-04-22 15:33:07 +0100

    fix: add staticfiles to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
+    "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
     "profiles",
